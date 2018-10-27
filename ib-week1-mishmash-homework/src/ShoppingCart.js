2393bbf35e3015eab64e59b6df02395f8c9f17ef
David <d_middelveld@hotmail.com>
2018-10-27 11:28:02 +0200

    new

--- ib-week1-mishmash-homework/src/ShoppingCart.js
+++ ib-week1-mishmash-homework/src/ShoppingCart.js
@@ -1,1 +1,12 @@
 const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
+
+
+class Cart {
+    constructor(){
+    this.newCart = []
+    }
+
+    getItems() {
+    return this.newCart;
+    }
+}
