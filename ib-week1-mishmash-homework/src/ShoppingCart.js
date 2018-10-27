30f81a52c3898e7d78ef49c819f7cd5feb7a8e83
David <d_middelveld@hotmail.com>
2018-10-27 19:53:19 +0200

    commit

--- ib-week1-mishmash-homework/src/ShoppingCart.js
+++ ib-week1-mishmash-homework/src/ShoppingCart.js
@@ -1,14 +1,41 @@
-const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
 
-
-class Cart {
-    constructor(){
-    this.newCart = []
-    }
+    class ShoppingCart {
+        constructor(cart) {
+        this.cart = cart
+        this.newCart = []
+        }
 
     getItems() {
-    return this.newCart;
+        return this.newCart;
+        }
+    
+
+    addItem(itemName, quantity, price) {
+        let newObj = {'name': itemName, 'quantity': quantity, 'pricePerUnit': price}
+        this.newCart.push(newObj) 
+        console.log(newCart)
+        
+        
+    }
+
+
+    clear() {
+        return this.newCart.length = 0
+    }
+
+
+    cartTotal() {
+        return this.getItems()
     }
 }
 
-module.exports.Cart = Cart
+
+
+
+
+
+
+module.exports = ShoppingCart
+
+//const cart = new ShoppingCart()
+//cart.getItems();
--- ib-week1-mishmash-homework/src/transform.js
+++ ib-week1-mishmash-homework/src/transform.js
@@ -1,5 +1,21 @@
-const groupAdultsByAgeRange = () => {
+const groupAdultsByAgeRange = (array) => {
+    const result1 = {};
+    const result2 = {};
+    const result3 = {};
+    const result4 = {};
+    const result5 = {};
 
+   array.filter(x => {
+    if (x.age > 18 && x.age < 20) {
+        result1
+
+    }
+
+   })
+
+
+
+    
 
 }
 
